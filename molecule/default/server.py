0bfdd6a45dbe5142af0d663dce3aab6a55f92752
stenh0use <james.stenhouse@gmail.com>
2022-12-13 16:16:10 -0500

    updated flask app with support for all codes

--- molecule/default/server.py
+++ molecule/default/server.py
@@ -2,24 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-headers = {
-    'Content-Type': 'application/json'
-}
+@app.route('/<int:code>')
+def status(code):
+    return '{"hello": "world"}', code, {'Content-Type': 'application/json'}
 
-body = '{ "hello": "world" }'
-
-@app.route("/200")
-def sc_200():
-    return (body, 200, headers)
-
-@app.route("/429")
-def sc_429():
-    return (body, 429, headers)
-
-@app.route("/501")
-def sc_501():
-    return (body, 501, headers)
-
-@app.route("/503")
-def sc_503():
-    return (body, 503, headers)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
